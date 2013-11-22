42f7d175eb1b350b8afd26c6bc7752e3fb2f043f
Mahmoud Abdelkader <mahmoud@balancedpayments.com>
2013-11-22 14:34:45 -0800

    finish up the role-nat

--- Berksfile
+++ Berksfile
@@ -1,13 +1,5 @@
-site :opscode
+# -*- mode: ruby -*-
+chef_api :config
 
-cookbook 'apt', tag: '2.2.0'
-cookbook 'rsyslog', tag: '1.9.0'
-cookbook 'munin', tag: '1.9.1'
-cookbook 'newrelic-sysmond', tag: '1.3.3'
-cookbook 'postfix', tag: '3.0.4'
-
-cookbook 'balanced-user', path: '../balanced-user'
-cookbook 'monit', github: 'phlipper/chef-monit', tag: '1.3.3'
-cookbook 'balanced-monit', path: '../balanced-monit'
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -7,10 +7,14 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 
-depends          'apt', '~> 2.2.0'
+depends          'apt', '~> 2.3.0'
+
+# role cookbooks
 depends          'newrelic-sysmond', '~> 1.3.3'
 depends          'rsyslog', '~> 1.9.0'
 depends          'munin', '~> 1.4.0'
 depends          'postfix', '~> 3.0.4'
+
+# balanced cookbooks
 depends          'balanced-monit', '~> 0.1.0'
 depends          'balanced-user', '~> 0.1.0'
